49692f089e6a19f57959058f64c9b6454c8152cc
coffeeman1a <coffeeman1a2k18@gmail.com>
2025-05-11 02:48:50 +0300

    added confirmation of luks password

--- arch_install.sh
+++ arch_install.sh
@@ -153,7 +153,7 @@ fi
 if [[ "$crypto" == true ]]; then
     read -rsp "Enter luks password: " luks_pw; echo
     read -rsp "Repeat luks password: " luks_pw_test; echo
-    if [[ "$luks_pw" != "$luks_pw_test"]]; then
+    if [[ "$luks_pw" != "$luks_pw_test" ]]; then
         echo "Password mismatch"
         exit 0
     fi
