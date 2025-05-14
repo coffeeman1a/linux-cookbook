c5afec7e36d1452cd3d45711503dbe72119ea28e
coffeeman1a <coffeeman1a2k18@gmail.com>
2025-05-14 07:32:53 +0300

    fix

--- arch_install.sh
+++ arch_install.sh
@@ -232,7 +232,6 @@ echo "Hostname: $hostname"
 lsblk
 read -rp "Enter target disk (for example, /dev/sda): " target; echo
 select_fs()
-
 read -rsp "Enter root password: " root_pw; echo
 read -rsp "Repeat root password: " root_pw_test; echo
 if [[ "$root_pw" != "$root_pw_test" ]]; then 
